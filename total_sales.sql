b92e08445c9dd86e0e3772a096c1e1bf556431b8
monikpatidar93-a11y <monikpatidar93@gmail.com>
2026-02-02 00:18:47 -0800

    Add files via upload

---
+++ total_sales.sql
@@ -0,0 +1,13 @@
+USE SALES_COMPANY;
+CREATE TABLE sale_product(
+product VARCHAR(20),
+categor VARCHAR(20),
+month VARCHAR(20),
+region VARCHAR(20),
+sales INT
+);
+SELECT * FROM sale_product;
+-- total sales
+
+SELECT SUM(sales) AS Total_sales
+FROM sale_product;
