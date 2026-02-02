1550ed3413e368390228183e7b99fd7702aeff57
monikpatidar93-a11y <monikpatidar93@gmail.com>
2026-02-01 23:48:31 -0800

    Create sales database and table with comments
    Added comments for clarity and corrected typos in SQL script.

--- CREATE_TABLE.sql
+++ CREATE_TABLE.sql
@@ -1,9 +1,23 @@
+/* 
+project : sales data analysis
+author monik patidar
+Description: this SQL script create the sales DATABASE and table
+*/
+
+--stap 1: create database
+CREATE DATABASE SALES_COMPANY;
+
+--step 2: use the database
 USE SALES_COMPANY;
+
+--stap 3: create sales table
 CREATE TABLE sale_product(
-product VARCHAR(20),
-categor VARCHAR(20),
-month VARCHAR(20),
-region VARCHAR(20),
-sales INT
+product VARCHAR(20),  -- name of the product
+categor VARCHAR(20),  -- product catagory
+month VARCHAR(20),    -- month of sale
+region VARCHAR(20),   -- sale region
+sales INT             -- sales of product
 );
+
+-- stap 4: display all data
 SELECT * FROM sale_product;
